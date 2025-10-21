1c460741177bbf59c93904aae13103db4f07a568
Joud Shaieb <joudshaieb524@gmail.com>
2025-10-21 21:20:55 +0400

    add more clients

--- IdentityServer/IdentityServerConfig.cs
+++ IdentityServer/IdentityServerConfig.cs
@@ -6,15 +6,49 @@ public static class IdentityServerConfig
     {
         return new List<Client>
         {
+            // 1. Machine-to-Machine Client (Service-to-Service)
             new Client
             {
-                ClientId = "client",
+                ClientId = "machine-client",
+                ClientName = "Machine to Machine Client",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets =
                 {
-                    new Secret("secret".Sha256())
+                    new Secret("machine-secret".Sha256())
                 },
                 AllowedScopes = { "api1" }
+            },
+
+            // 2. Web Application Client (with user login)
+            new Client
+            {
+                ClientId = "web-client",
+                ClientName = "Web Application Client",
+                AllowedGrantTypes = GrantTypes.Code,
+                ClientSecrets =
+                {
+                    new Secret("web-secret".Sha256())
+                },
+                RedirectUris = { "https://localhost:5001/signin-oidc" },
+                PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc" },
+                AllowedScopes = { "openid", "profile", "api1" },
+                RequireConsent = false
+            },
+
+            // 3. Single Page Application (SPA) Client
+            new Client
+            {
+                ClientId = "spa-client",
+                ClientName = "Single Page Application Client",
+                AllowedGrantTypes = GrantTypes.Code,
+                RequireClientSecret = false, // SPAs can't securely store secrets
+                RequirePkce = true, // Security enhancement for SPAs
+                RedirectUris = { "http://localhost:3000/callback" },
+                PostLogoutRedirectUris = { "http://localhost:3000" },
+                AllowedCorsOrigins = { "http://localhost:3000" },
+                AllowedScopes = { "openid", "profile", "email", "api1" },
+                RequireConsent = false,
+                AllowAccessTokensViaBrowser = true
             }
         };
     }
@@ -32,7 +66,8 @@ public static class IdentityServerConfig
         return new List<IdentityResource>
         {
             new IdentityResources.OpenId(),
-            new IdentityResources.Profile()
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
         };
     }
 
